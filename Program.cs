0acf3288696a2cb1cb127f85122eb034cb30e94f
sargently <ramirezcastillogaby@gmail.com>
2026-02-02 11:33:53 -0400

    declarar variables
    declarando e imprimiendo variables en la consola

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Gabriela A. Ramirez Castillo
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int number = 20;
+        float decimalNumber = 20.5f;
+        string text = "Hello, World!";
+        bool isActive = true;
+        char character = 'A';
+        double largeDecimal = 123456.789;
+        long bigNumber = 12345678901234;
+        byte smallNumber = 255;
+
+        Console.WriteLine($"Integer: {number}");
+        Console.WriteLine($"Float: {decimalNumber}");
+        Console.WriteLine($"String: {text}");
+        Console.WriteLine($"Boolean: {isActive}");
+        Console.WriteLine($"Character: {character}");
+        Console.WriteLine($"Double: {largeDecimal}");
+        Console.WriteLine($"Long: {bigNumber}");
+        Console.WriteLine($"Byte: {smallNumber}");
+
+    }
+}
